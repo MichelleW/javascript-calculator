f2f94534fe07eabb1e6924de5fbffcb9ca60d02a
Michelle <michelle.wu177@gmail.com>
2016-07-21 14:00:07 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is the local copy of the calculator project from CodeAcademy.
---
+++ scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length - 9, 9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
+    };
+    var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+
+    $("#numbers > a").not("#clear,#clearall").click(function() {
+        number += $(this).text();
+        totaldiv.text(number);
+        testNumLength(number);
+    });
+    //Add another .click() here!
+    $("#operators a").not("#equals").click(function() {
+        operator = $(this).text();
+    });
+
+});
