b9520387d766e76cd80ea7e2058ac84c00752cd7
Michelle <michelle.wu177@gmail.com>
2016-07-21 15:18:24 -0700

    jQuery version working

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 This is the local copy of the calculator project from CodeAcademy.
+* Working in jQuery
+* need to convert to raw js
--- scripts.js
+++ scripts.js
@@ -13,15 +13,37 @@ $(document).ready(function() {
     var operator = "";
     var totaldiv = $("#total");
     totaldiv.text("0");
-
-    $("#numbers > a").not("#clear,#clearall").click(function() {
+    $("#numbers a").not("#clear,#clearall").click(function() {
         number += $(this).text();
         totaldiv.text(number);
         testNumLength(number);
     });
-    //Add another .click() here!
     $("#operators a").not("#equals").click(function() {
         operator = $(this).text();
+        newnumber = number;
+        number = "";
+        totaldiv.text("0");
     });
-
+    $("#clear,#clearall").click(function() {
+        number = "";
+        totaldiv.text("0");
+        if ($(this).attr("id") === "clearall") {
+            newnumber = "";
+        }
+    });
+    $("#equals").click(function() {
+        if (operator === "+") {
+            number = (parseInt(number, 10) + parseInt(newnumber, 10)).toString();
+        } else if (operator === "-") {
+            number = (parseInt(newnumber, 10) - parseInt(number, 10)).toString();
+        } else if (operator === "/") {
+            number = (parseInt(newnumber, 10) / parseInt(number, 10)).toString();
+        } else if (operator === "*") {
+            number = (parseInt(newnumber, 10) * parseInt(number, 10)).toString();
+        }
+        totaldiv.text(number);
+        testNumLength(number);
+        number = "";
+        newnumber = "";
+    });
 });
